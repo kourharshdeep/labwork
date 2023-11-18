0938438c6ae386065d21a77c214c6354b1676ea4
kourharshdeep <146852680+kourharshdeep@users.noreply.github.com>
2023-11-18 19:58:47 +0530

    Update and rename helloworldtab.c to experiment1(b).c
    program to print hello world using new line operator.

---
+++ experiment1(b).c
@@ -0,0 +1,6 @@
+//program for hello world using new line operator.
+#include<stdio.h>
+void main()
+{
+printf("Hello world/n hello world");
+}
--- helloworldtab.c
+++
@@ -1,6 +0,0 @@
-//program for hello world using new tab operator.
-#include<stdio.h>
-void main()
-{
-printf("Hello world\t hello world");
-}
